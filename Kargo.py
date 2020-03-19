53d8526b62e72055ea1f7c0c05502431a7cf4185
Harshil-Sharma <62276864+Harshil-Sharma@users.noreply.github.com>
2020-03-18 20:02:44 -0400

    Kargo.py
    This file contains the function that maps two strings. The function works for the condition where the length of the s1 string is less than or equal to the length of the s2 string (len(s1) <= len(s2)).

---
+++ Kargo.py
@@ -0,0 +1,13 @@
+import sys
+
+def mapping(s1, s2):
+	if len(s1) <= len(s2):
+		d = {}
+		for i in range(len(s1)):
+			if s1[i] not in d:
+				d[s1[i]] = s2[i]
+			if d[s1[i]] != s2[i]:
+				return False
+		return True
+	return False
+  
